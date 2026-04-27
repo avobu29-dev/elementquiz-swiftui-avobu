43aa0991bfd50db805aa897303e63ac792cbdc32
avobu29-dev <avobu29@student.kis.or.kr>
2026-04-27 14:31:43 +0900

    Added comments
    Added comments explaining differences between SwiftUI and storyboard usage.

--- ViewController.swift
+++ ViewController.swift
@@ -1,14 +1,14 @@
-import SwiftUI
+import SwiftUI // In swift storyboard, part of the code is written and part of it is the storyboard file. However in SwiftUI, the whole app is written in code.
 
-struct ContentView: View {
+struct ContentView: View { //There is no viewcontroller; there is a view struct instead which defines how the UI looks.
     
     let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
     
-    @State private var currentElementIndex = 0
+    @State private var currentElementIndex = 0 //In storyboard, elements are manually updated. In SwiftUI, it automatically redraws the UI.
     @State private var answerText = "?"
     
     var body: some View {
-        VStack(spacing: 20) {
+        VStack(spacing: 20) { //Storyboard uses constraints, while SwiftUI uses layout stacks which are much simpler.
             
             Image(elementList[currentElementIndex])
                 .resizable()
@@ -20,7 +20,7 @@ struct ContentView: View {
             
             HStack {
                 
-                Button("Show Answer") {
+                Button("Show Answer") { //In storyboard, the elements such as buttons are connected with IBActions. However, in SwiftUI buttons contain their actions directly.
                     answerText = elementList[currentElementIndex]
                 }
                 .buttonStyle(.borderedProminent)
